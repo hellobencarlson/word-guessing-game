c8a5122761a5e3ba4a73863bddb86bb2dd13b888
--unset <hellobencarlson@gmail.com>
2022-06-07 23:47:28 -0400

    continuing to build js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
+// to do
+// X clicks & 
+// keying register whats selected
+// display entry in boxes on first row
+// thern check for match on enter
+// run only on enter
+// then move to lower rows
+
 
 const phrases = ["what does the fox say", "happy birthday", "good morning", "hey there", "how are you"]
 
+// GET RANDOM PHRASE
 
 function getRandomPhrase() {
     // get a number between 0-4
@@ -8,9 +17,64 @@ function getRandomPhrase() {
         // random is 0-1, multiply times 5 makes potential 0-4 (arrray of 5)
     console.log(randomNumber);
     // get the phrase from the array with that #
-    var phraseReturn = phrases[randomNumber];
+    phraseReturn = phrases[randomNumber];
     // return phraseReturn;
     console.log(phraseReturn);
+    
 }
 
 getRandomPhrase();
+
+
+// ADD PHRASE TO DISPLAY
+
+function addPhraseToDisplay() {
+    // var newPhrase = this.phrase;
+    var stringPhrase = JSON.stringify(phraseReturn);
+    console.log(stringPhrase);
+    // for each letter in phrase- add li
+    for (var i = 0; i < stringPhrase.length; i++) {
+        // get each letter in the phrase
+        let currentLetter = stringPhrase.charAt(i);
+         console.log(currentLetter);
+        // if it's not quotes
+         if (currentLetter != '"') {
+            // create li 
+             let newLi = document.createElement('li');
+             // add Li with space class if space
+            // indexOf meaning contains " " (space) and at currentLetter (0)
+           if (currentLetter == " ") {
+              
+               newLi.classList.add("space");
+             } else {
+        //          if not a space, add  hidden letter
+                newLi.classList.add("hide");
+                 newLi.classList.add("letter");
+                 // letter to match from class
+                 newLi.classList.add(currentLetter);
+             }
+            // add Li to UL
+            document.getElementsByTagName('ul')['0'].appendChild(newLi);
+         }
+     }
+}
+ 
+
+ addPhraseToDisplay();
+
+ // COLLECT SELECTED KEY
+
+
+
+ function collectClick() {
+    var clicked = event.target
+   if (clicked.tagName = "BUTTON") {
+       console.log(clicked.innerHTML);
+       document.getElementById('mystery-word').firstElementChild.firstElementChild.innerHTML = clicked.innerHTML;
+    // add loop
+    }
+    // if (button.tagName == "BUTTON" && button.className != "key wrong" && button.className != "key chosen") {  
+    //     game.handleInteraction(button);
+}
+
+document.getElementById('qwerty').addEventListener("click", collectClick);
